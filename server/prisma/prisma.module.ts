1029dbd38a1221afe51f51c887c9b28789063bb8
abdull-07 <personal.muhammadabdullah@gmail.com>
2026-07-12 12:43:27 +0500

    start backend

--- server/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // makes PrismaService available everywhere without importing PrismaModule each time
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
--- server/src/product/dto/update-product.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductDto } from './create-product.dto';
-
-// makes every field from CreateProductDto optional automatically
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
